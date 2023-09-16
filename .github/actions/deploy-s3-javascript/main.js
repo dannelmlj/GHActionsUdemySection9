5ab32a4a79a8fe132de95a398764de9ffde064e6
dannelmlj <dannelmuljaa@gmail.com>
2023-09-17 03:17:46 +0800

    connect to s3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,14 @@ const exec = require('@actions/exec');
 
 
 function run(){
+    // Get Input Values
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('bucket-region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+    // Upload files
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`)
+
     core.notice("Hello from JavaScript")
 }
 
